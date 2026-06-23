61d6f875912e8a3f7ca2ef43189ced0938191030
elaraby1234 <aradm8477@gmail.com>
2026-06-23 21:46:26 +0300

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:20 as development
+WORKDIR /app
+COPY package.json .
+RUN npm install
+COPY . .
+EXPOSE 4000  
+CMD [ "npm" , "run" , "start-dev" ] 
+
+FROM node:20 as production
+WORKDIR /app
+COPY package.json .
+RUN npm install --only=production
+COPY . .
+EXPOSE 4000  
+CMD [ "npm" , "start"] 
---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const redis = require('redis');
+
+const PORT = 4000;
+const app = express();
+// connect to redis
+const redis_port = 6379;
+const redis_host = 'redis';
+const redisclient = redis.createClient({
+    url: `redis://${redis_host}:${redis_port}`,
+});
+redisclient.on('error', err => console.log('Redis Client Error', err));
+redisclient.on('connect', ()=> console.log('connected to redis....'));
+redisclient.connect();
+// connect to mongodb
+const DB_NAME= 'root';
+const DB_PASSWOED = 'example';
+const DB_PORT = 27017 ;
+const DB_HOST = '172.18.0.2';
+
+
+const URI = `mongodb://${DB_NAME}:${DB_PASSWOED}@${DB_HOST}:${DB_PORT}`;
+
+mongoose.connect(URI).then(() => console.log("connected.")).catch(() => console.log("failed to connect"));
+
+app.get('/', (req,res) => res.send('<h1> Hello Tresmerge!dev dev</h1>'));
+app.listen(PORT , () =>console.log(`app is up and running on port: ${PORT}`));
